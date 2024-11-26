954fd88d7e09b36b8dbe79e57fb69374c80a9168
JaviiCode <jgmederos10@gmail.com>
2024-11-27 00:38:54 +0100

    Sistema de descuento a las multas actualizado. mostrar datos del conductor completo.

--- JavaScript/act46/Conductor.js
+++ JavaScript/act46/Conductor.js
@@ -1,8 +1,10 @@
 export class Conductor{
-    constructor(id,Nombre,FechaNacimiento){
+    constructor(id, Nombre, FechaNacimiento, Monto, MontoConDescuento){
         this.id = id;
         this.Nombre = Nombre;
-        this.FechaNacimiento = FechaNacimiento; 
+        this.FechaNacimiento = FechaNacimiento;
+        this.Monto = Monto;
+        this.MontoConDescuento = MontoConDescuento;  
         this.vehiculos = [];
         this.Infracciones = [];
     }
--- JavaScript/act46/index.js
+++ JavaScript/act46/index.js
@@ -25,26 +25,41 @@ conductor1.vehiculos.push(vehiculo1);
 
 let infraccion1 = new Infraccion(1, "Exceso de velocidad", 25);
 let infraccion2 = new Infraccion(2, "Salto de stop", 40);
-let infraccion3 = new Infraccion(3, "Ecceso de alcohol", 120);
-let infraccion4 = new Infraccion(4, "Ecceso de drogas", 200);
+let infraccion3 = new Infraccion(3, "Exceso de alcohol", 120);
+let infraccion4 = new Infraccion(4, "Exceso de drogas", 200);
 
 conductor1.Infracciones.push(infraccion1, infraccion2, infraccion3);
-console.log(conductor1);
 
-    if(conductor1.Infracciones.length > 3){
-        console.log(`El conductor ${conductor1.Nombre} no tiene derecho a descuento`);
-    }
+    //Condicionales para calcular el descuento total.
     if(conductor1.Infracciones.length > 0 && conductor1.Infracciones.length <= 3){
-        console.log("jols");
         let sumaInfracciones = 0;
         for(let i = 0; i < conductor1.Infracciones.length; i++){
-            let infratest = conductor1.Infracciones[i].Monto;
             sumaInfracciones += conductor1.Infracciones[i].Monto;
 
         }
+        conductor1.Monto = sumaInfracciones;
         let montoFinal = sumaInfracciones - (sumaInfracciones * 20 / 100);
+        conductor1.MontoConDescuento = montoFinal;
+
         console.log(`El conductor ${conductor1.Nombre} tiene un 20% de descuento en sus infracciones. La multa total se le queda en ${montoFinal}`);
     }
+    if(conductor1.Infracciones.length == 1){
+        let sumaInfracciones = 0;
+        for(let i = 0; i < conductor1.Infracciones.length; i++){
+            sumaInfracciones += conductor1.Infracciones[i].Monto;
+
+        }
+        conductor1.Monto = sumaInfracciones;
+        let montoFinal = sumaInfracciones - (sumaInfracciones * 50 / 100);
+        conductor1.MontoConDescuento = montoFinal;
+        console.log(`El conductor ${conductor1.Nombre} tiene un 50% de descuento en sus infracciones. La multa total se le queda en ${montoFinal}`);
+    }
+    if(conductor1.Infracciones.length >3){
+        console.log(`El conductor ${conductor1.Nombre} no tiene derecho a descuento`);
+    }
+    console.log("Datos del conductor.");
+    console.log(conductor1);
+
     
 
 
