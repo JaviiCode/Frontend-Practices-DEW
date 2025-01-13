5344ff3d7cc7ca615bc27cc1c5ca6d78462753d0
JaviiCode <jgmederos10@gmail.com>
2025-01-13 14:40:24 +0100

    add filters

---
+++ JavaScript/AA_61/Registros.js
@@ -0,0 +1,9 @@
+export class Registros{
+    constructor(id, fecha, concepto, cantidad, caja){
+        this.id = id;
+        this.fecha = fecha;
+        this.concepto = concepto;
+        this.cantidad = cantidad;
+        this.caja = caja;
+    }
+}
--- JavaScript/AA_61/index.js
+++ JavaScript/AA_61/index.js
@@ -1,47 +1,52 @@
+import {Registros} from '/Registros.js';
+
 const regisroTabla = document.getElementById('regisroTabla').getElementsByTagName('tbody')[0];
-const registros = [];
+const conjuntoRegistros = [];
+let contador = 0;
 
 document.getElementById("btnAgregar").addEventListener("click", (e) => {
-    let contador = 0;
-    contador++;
-    e.preventDefault();
-    /*const fechaInput = fecha.value;
-    const conceptoInput = concepto.value;
-    const cantidadInput = cantidad.value;
-    const cajaDrop = caja.value;
-    console.log(fechaInput);*/
+    e.preventDefault()
+    
+    
     let fecha = document.getElementById("fecha").value;
     let concepto = document.getElementById("concepto").value;
     let cantidad = document.getElementById("cantidad").value;
-    let caja = document.getElementById("caja");
+    let caja = document.getElementById("caja").value;
     let Formulario = document.getElementById("Formulario").value;
 
-    registros.push(contador, fecha, concepto, cantidad, caja);
+    
+    
+
+    
     const nuevaFila = regisroTabla.insertRow();
     nuevaFila.insertCell().textContent = contador;
     nuevaFila.insertCell().textContent = fecha;
     nuevaFila.insertCell().textContent = concepto;
     nuevaFila.insertCell().textContent = cantidad;
-    nuevaFila.setAttribute.textContent = caja;
-    //Formulario.reset();
+
+    //Guardo informacion en un array
+    contador++;
+    let registro = new Registros(contador, fecha, concepto, cantidad, caja);
+    conjuntoRegistros.push(registro);
+    console.log(conjuntoRegistros);
+
 })
 
 
 
-document.getElementById("filtrar").addEventListener("click", (e) => {
-    let filtroConcepto = document.getElementById("concepto").value;
-    let filtroCaja = document.getElementById("caja").value;
+document.getElementById("filtrar").addEventListener("click", () => {
+    let filtroConcepto = document.getElementById("conceptoFiltro").value;
+    let filtroCaja = document.getElementById("cajaFiltro").value;
 
     regisroTabla.innerHTML = "";
-
     // Iterar sobre los registros
-    registros.forEach((registro) => {
-        console.log("hola entre");
-        if (registro.concepto === filtroConcepto && registro.caja === filtroCaja) {
+    conjuntoRegistros.forEach((registro) => {
+        console.log(registro.concepto);
+        if (registro.concepto=== filtroConcepto && registro.caja === filtroCaja) {
             // Crear una nueva fila
             const nuevaFila = regisroTabla.insertRow();
 
-            nuevaFila.insertCell().textContent = registro.contador; 
+            nuevaFila.insertCell().textContent = registro.id; 
             nuevaFila.insertCell().textContent = registro.fecha;
             nuevaFila.insertCell().textContent = registro.concepto;
             nuevaFila.insertCell().textContent = registro.cantidad;
@@ -49,5 +54,22 @@ document.getElementById("filtrar").addEventListener("click", (e) => {
     });
 
 
-})
+});
+
+document.getElementById("restaurar").addEventListener("click", () => {
+
+
+    regisroTabla.innerHTML = "";
+    // Iterar sobre los registros
+    conjuntoRegistros.forEach((registro) => {;
+        
+            const nuevaFila = regisroTabla.insertRow();
+
+            nuevaFila.insertCell().textContent = registro.id; 
+            nuevaFila.insertCell().textContent = registro.fecha;
+            nuevaFila.insertCell().textContent = registro.concepto;
+            nuevaFila.insertCell().textContent = registro.cantidad;
+    });
+});
+
 
